cde0b52d13fc885ebc69dea2c720316a765c9b3a
kargbu <kargb@ciencias.unam.mx>
2024-11-01 19:45:03 -0300

    Subir, clases

---
+++ clase_3/1.ciclos.js
@@ -0,0 +1,11 @@
+// Ciclo for se utiliza para repetir un bloque de código un número específico de veces. 
+for (j = 1; j < 4; j++) {
+    console.log(j);
+}
+
+// Ciclo con while, se ejecuta mientras una condición es verdadera
+let i = 1
+while (i < 5) {
+    console.log(i);
+    i++;
+}
