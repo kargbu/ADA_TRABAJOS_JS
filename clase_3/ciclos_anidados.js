15598074d6140d03ec8f960d2706a402eaa8c261
kargbu <kargb@ciencias.unam.mx>
2024-11-05 19:55:59 -0300

    Otra vez, vámos a subir

--- clase_3/ciclos_anidados.js
+++ clase_3/ciclos_anidados.js
@@ -3,7 +3,9 @@
 for (var factor_uno = 0; factor_uno < 2; factor_uno++) {
    // Esto significa que el bucle for comienza con 0 y se detiene después de 1.
    for (var factor_dos = 0; factor_dos < 2; factor_dos++) {
-    console.log(factor_uno + 'x' + factor_dos + ' = ' + factor_uno * factor_dos);
+    console.log(factor_uno + ' x ' + factor_dos + ' = ' + factor_uno * factor_dos);
     }
 }
+// Tabla para dividir
+
 
