78db3ed3381420c3c13ba91ee74005b637702bd3
kargbu <kargb@ciencias.unam.mx>
2024-11-05 19:54:14 -0300

    Vámos

---
+++ clase_3/ciclos_anidados.js
@@ -0,0 +1,9 @@
+// Crear una tabla de multiplicar
+
+for (var factor_uno = 0; factor_uno < 2; factor_uno++) {
+   // Esto significa que el bucle for comienza con 0 y se detiene después de 1.
+   for (var factor_dos = 0; factor_dos < 2; factor_dos++) {
+    console.log(factor_uno + 'x' + factor_dos + ' = ' + factor_uno * factor_dos);
+    }
+}
+
