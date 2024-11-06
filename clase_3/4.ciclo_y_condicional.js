4dc2c6cadb4edf9a5effecfb5d6856614b1e7282
kargbu <kargb@ciencias.unam.mx>
2024-11-05 21:30:11 -0300

    Van cambios

---
+++ clase_3/4.ciclo_y_condicional.js
@@ -0,0 +1,31 @@
+// Programar un ciclo con un condicional.
+
+for(var i = 1; i <= 10; i++) {
+    if (i == 1) {
+        console.log('Medalla de oro')
+    } else if (i == 2) {
+        console.log('Medalla de plata')
+    } else if(i == 3) {
+        console.log('Medalla de bronce')
+    } else {
+        console.log(i);
+    }
+}
+
+// Programar con el condicional switch
+
+for(var i = 1; i <= 10; i++) {
+    switch(i) {
+    case 1:
+        console.log('Medalla de oro');
+    break;
+    case 2:
+        console.log('Medalla de plata');
+    break;
+    case 3:
+        console.log('Medalla de bronce');
+    break;
+    default:
+        console.log(i);
+    }
+}
---
+++ clase_3/5.ejercicio.js
@@ -0,0 +1,68 @@
+/*
+var i
+
+if(i == 0 && i == 1) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+}
+
+var i = 0;
+while(i < 3) {
+  console.log("Hello");
+  i++;
+}
+
+for (i = 0; i < 2; i++) {
+    for (var j = 0; j < 3; j++) {
+        console.log("Hello");
+    }
+}
+
+var i = 7
+if(i <= 5) {
+    console.log("Hello");
+  } else if(i <= 10) {
+    console.log("Goodnight");
+  } else {
+    console.log("Goodbye");
+  }
+
+var i = 3
+  switch(i) {
+    case 1:
+      console.log("Hello");
+      break;
+    case 2:
+      console.log("Goodnight");
+      break;
+    case 3:
+      console.log("Goodbye");
+      break;
+  }
+
+var i = 3
+  if(i == 2 || i == 3) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+
+  var i = 3;
+  var j = 5;
+
+  if(i == 3 && j < 5) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+*/
+  var i = 7;
+  var j = 2;
+
+  if(i < 7 || j < 5) {
+    console.log("Hello");
+  } else {
+    console.log("Goodbye");
+  }
+
